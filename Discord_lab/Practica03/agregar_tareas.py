693858ccec9af6b557a23af4acca7d86ac3d5898
Juan <cesar180687@hotmail.com>
2026-05-11 08:46:13 -0600

    Definicion de tareas 2.0

--- Discord_lab/Practica03/agregar_tareas.py
+++ Discord_lab/Practica03/agregar_tareas.py
@@ -13,3 +13,58 @@ def agregar_tarea(lista_tareas, descripcion):
     nueva_tarea = f"{descripcion} - {fecha}"
     lista_tareas.append(nueva_tarea)
     return f"Tarea agregada con éxito"
+
+def listado_tareas(lista_tareas):
+    '''
+    Devuelve un listado de tareas formateado
+    '''
+    if not lista_tareas:
+        return "No hay tareas pendientes"
+    
+    #agrega una variable llamada resultado
+    resultado = "Listado de tareas:\n"
+
+    #Iterar la lista de tareas y formatear la salida
+    for i, tarea in enumerate(lista_tareas, 1):
+        resultado += f"{i}. {tarea}\n"
+    return resultado.strip()
+
+def eliminar_tarea(lista_tareas, indice):
+    '''
+    Elimina una tarea de la lista según su índice
+    '''
+    if not indice.isdigit():
+        return "Error: El índice debe ser un número"
+    
+    indice = int(indice)-1
+
+    #Agregamos la logica para preguntar
+    #Si el elemento esta en la lista y eliminarlo
+    if 0 <= indice < len(lista_tareas):
+        tarea_eliminada = lista_tareas.pop(indice)
+    else:
+        return "Error: Índice fuera de rango"
+    return f"Tarea eliminada: {tarea_eliminada}"
+
+def main():
+    tareas = []
+    PREFIJO = "!"
+
+    print("Bienvenido al gestor de tareas")
+    activa = True
+    while activa:
+        entrada = input(">>> ").strip()
+
+        if not entrada.startswith(PREFIJO):
+            print("Error: Comando no reconocido")
+            continue
+
+        #Procesamiento de la entrada
+        cuerpo = entrada[len(PREFIJO):].split(maxsplit=1)
+        comando = cuerpo[0].lower()
+        argumento = cuerpo[1] if len(cuerpo) > 1 else ""
+
+        #Seleccion de accion
+        if comando == "add":
+            resultado = agregar_tarea(tareas, argumento)
+            print
